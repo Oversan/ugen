b97dee589f3ec53bec21de34e4db4a28bdff07b3
Anton Borsch <anton.borsch@gmail.com>
2016-04-17 02:48:29 +0300

    Add boilerplate folder for tests.

---
+++ tests/fixtures/ugen.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "questions": [
+    {
+      "name": "",
+      "type": "input",
+      "message": "1. Question message or function ()",
+      // "validate": "Validation function there"
+    }
+  ],
+  "templatesVariables": [],
+  "boilerplatePath": "/Users/Oversan/projects/ugen/tests/fixtures/boilerplate"
+}
