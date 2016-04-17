3d32490478cefc356a39c442e6e1165035641a5e
Anton Borsch <anton.borsch@gmail.com>
2016-04-17 03:02:14 +0300

    Added test for createConfig.

---
+++ tests/createConfig.test.js
@@ -0,0 +1,19 @@
+const path = require('path')
+const fs = require('fs')
+const exec = require('child_process').exec
+const expect = require('chai').expect
+const fixturesPath = path.join(process.cwd(), './tests/fixtures')
+const boilerplatePath = path.join(process.cwd(), './tests/fixtures/boilerplate')
+const createConfig = require('../lib/createConfig.js')
+
+describe('createConfig()', () => {
+  it('should create config from path with boilerplate folder without templates', () => {
+    process.chdir('./tests/fixtures')
+    createConfig(boilerplatePath)
+    const resultData = require(path.join(process.cwd(), './ugen.config.js'))
+    const correctData = require(path.join(process.cwd(), './expected.ugen.config.js'))
+    expect(JSON.stringify(resultData)).to.be.eql(JSON.stringify(correctData))
+    process.chdir('../../')
+    fs.unlinkSync('./tests/fixtures/ugen.config.js')
+  })
+})
