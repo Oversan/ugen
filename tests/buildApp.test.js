76c92a4a31ba75d76a569b6203e9bf41bfcb05aa
Anton Borsch <anton.borsch@gmail.com>
2016-04-19 23:27:03 +0300

    Modify buildApp test. Create config from boilerplate.

--- tests/buildApp.test.js
+++ tests/buildApp.test.js
@@ -3,24 +3,34 @@ const fs = require('fs-extra')
 const currentCwd = process.cwd()
 const expect = require('chai').expect
 const buildApp = require('../lib/buildApp.js')
-const config = require('./fixtures/oneTemplate/ugen.config.js')
+const createConfig = require('../lib/createConfig.js')
+const boilerplateWithOneTemplatePath = path.join(process.cwd(), './tests/fixtures/oneTemplate/boilerplate')
 const bddStdin = require('bdd-stdin')
 
 it('should create correct answers object and file structure', function(done) {
   process.chdir('./tests/fixtures/oneTemplate')
-  const expectedAnswers = { ENV_HOME: 'AppHomeDir' }
-  const procfilePath = path.join(process.cwd(), 'Procfile')
-  const karmaPath = path.join(process.cwd(), 'Karma.conf.js')
-  const gulpPath = path.join(process.cwd(), 'gulpfile.js')
-  const configPath = path.join(process.cwd(), 'config')
-  bddStdin('AppHomeDir\n')
-  buildApp(config).then((answers) => {
-    expect(answers).to.be.deep.equal(expectedAnswers)
-    fs.unlinkSync(procfilePath)
-    fs.unlinkSync(karmaPath)
-    fs.unlinkSync(gulpPath)
-    fs.removeSync(configPath)
-    done()
-  }).catch((err) => {console.log(`Test error ${err}`)})
-  process.chdir(currentCwd)
+
+  createConfig(boilerplateWithOneTemplatePath, () => {
+
+    const ugenConfigPath = path.join(process.cwd(), 'ugen.config.js')
+    const config = require(ugenConfigPath)
+    const expectedAnswers = { ENV_HOME: 'AppHomeDir' }
+    const procfilePath = path.join(process.cwd(), 'Procfile')
+    const karmaPath = path.join(process.cwd(), 'Karma.conf.js')
+    const gulpPath = path.join(process.cwd(), 'gulpfile.js')
+    const configPath = path.join(process.cwd(), 'config')
+
+    bddStdin('AppHomeDir\n')
+    buildApp(config).then((answers) => {
+      expect(answers).to.be.deep.equal(expectedAnswers)
+      fs.unlinkSync(procfilePath)
+      fs.unlinkSync(karmaPath)
+      fs.unlinkSync(gulpPath)
+      fs.removeSync(configPath)
+      fs.unlinkSync(ugenConfigPath)
+      done()
+    }).catch((err) => {console.log(`Test error ${err}`)})
+    process.chdir(currentCwd)
+  })
+
 })
