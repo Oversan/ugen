3f6b8e3c1785e56c7b18175116afd6f696f69ee6
Anton Borsch <anton.borsch@gmail.com>
2016-04-19 18:47:55 +0300

    Correct change cwd.

--- tests/buildApp.test.js
+++ tests/buildApp.test.js
@@ -6,17 +6,21 @@ const buildApp = require('../lib/buildApp.js')
 const config = require('./fixtures/oneTemplate/ugen.config.js')
 const bddStdin = require('bdd-stdin')
 
-it('should call save once', function(done) {
+it('should create correct answers object and file structure', function(done) {
   process.chdir('./tests/fixtures/oneTemplate')
   const expectedAnswers = { ENV_HOME: 'AppHomeDir' }
+  const procfilePath = path.join(process.cwd(), 'Procfile')
+  const karmaPath = path.join(process.cwd(), 'Karma.conf.js')
+  const gulpPath = path.join(process.cwd(), 'gulpfile.js')
+  const configPath = path.join(process.cwd(), 'config')
   bddStdin('AppHomeDir\n')
   buildApp(config).then((answers) => {
     expect(answers).to.be.deep.equal(expectedAnswers)
-    fs.unlinkSync(path.join(process.cwd(), 'Procfile'))
-    fs.unlinkSync(path.join(process.cwd(), 'Karma.conf.js'))
-    fs.unlinkSync(path.join(process.cwd(), 'gulpfile.js'))
-    fs.removeSync(path.join(process.cwd(), 'config'))
-    process.chdir(currentCwd)
+    fs.unlinkSync(procfilePath)
+    fs.unlinkSync(karmaPath)
+    fs.unlinkSync(gulpPath)
+    fs.removeSync(configPath)
     done()
   }).catch((err) => {console.log(`Test error ${err}`)})
+  process.chdir(currentCwd)
 })
