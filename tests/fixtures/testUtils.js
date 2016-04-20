9445cd17e352c4e88c879ec9d66a1f00a0ae4776
Anton Borsch <anton.borsch@gmail.com>
2016-04-20 14:40:19 +0300

    ‘filesArray’ is optional param for test util now.

--- tests/fixtures/testUtils.js
+++ tests/fixtures/testUtils.js
@@ -1,10 +1,19 @@
 const fs = require('fs-extra')
 const path = require('path')
+const glob = require('glob')
 
 function makeTestFolder(archiveFolderPath, appFolderPath, filesArray, options) {
   fs.mkdirs(appFolderPath)
 
-  filesArray.forEach((file) => {
+  const globOptions = {
+    cwd: archiveFolderPath,
+    dot: true,
+    ignore: '**/node_modules/**/*'
+  }
+
+  const files = filesArray || glob.sync("**/*", globOptions)
+
+  files.forEach((file) => {
     var archiveFilePath = path.join(archiveFolderPath, file)
     var boilerplateFolderPath = path.join(appFolderPath, 'boilerplate')
     var boilerplateFilePath = path.join(boilerplateFolderPath, file)
